843fb9b684ef7aacfeab57762612a2afdb04b3cd
Rich Shea <richcshea@gmail.com>
2014-12-16 17:30:20 -0700

    successfully record ideas

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
+require './idea'
+
 class IdeaBoxApp < Sinatra::Base
+
+  not_found do
+    erb :error
+  end
+
   get '/' do
-    "Hello, World! Here I am!"
+    erb :index
+  end
+
+  post '/' do
+    # 1.  Create an idea based on the form parameters
+    idea = Idea.new
+    # 2.  Store it
+    idea.save
+    # 3.  Send us back to the index page to see all ideas
+    "Creating an IDEA!"
   end
 end
---
+++ idea.rb
@@ -0,0 +1,5 @@
+class Idea
+
+  def save
+  end
+end
